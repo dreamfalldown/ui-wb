86a75716200a2405f8b07842baadc5a28fa304dc
dreamfalldown <1787833352@qq.com>
2021-05-17 11:39:46 +0800

    init page and tailwind

---
+++ src/pages/Layout/ToolBar.vue
@@ -0,0 +1,13 @@
+<template lang="">
+    <header class='h-1/5 bg-gray-800'>
+        heallo
+    </header>
+</template>
+<script>
+export default {
+    
+}
+</script>
+<style lang="">
+    
+</style>
